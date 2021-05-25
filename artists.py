d8ae57a4f57056931417d7b4d803eddf04285634
Sechaba Mokobane <sechaba.mokobane@gmail.com>
2021-05-25 16:19:59 +0200

    Added various elements including html code for the input

--- artists.py
+++ artists.py
@@ -1,2 +1,33 @@
 # The code will cover the design of a platform where the artist can post items and the user can 
 # search for artists.
+
+import tkinter
+
+
+window = tkinter.Tk()
+
+window.title("Artist")
+window.minsize(width=500,height=500)
+
+
+# Label
+artist_header = tkinter.Label(text="Artist interface",font = ("Arial",15,"bold"))
+artist_header.grid(column=2,row=3)
+
+artistname_label = tkinter.Label(text="Artist name",font = ("Arial",11))
+artistname_label.grid(column=1,row=3)
+
+# Button
+def continue_button():
+    artistname_label["text"] = "The button has been clicked"
+    artist_header["text"] = input.get()
+
+button = tkinter.Button(text="Continue",command=continue_button)
+#button.pack(side="left")
+
+# Entry
+input = tkinter.Entry()
+#input.pack(side="left")
+
+
+window.mainloop()
--- spotify_client.py
+++ spotify_client.py
@@ -4,26 +4,29 @@
 
 import pandas as pd
 from spotipy import SpotifyClientCredentials,oauth2,Spotify,util
-import sys,os
+import sys,os,webbrowser
 
-#export SPOTIPY_REDIRECT_URI=""
+import spotipy
+
 # authenticating my credentials
 cid = "702f99bae6f5489da997f6c86663956b"
 secret_id = "08d83f6fe0a0483799fb4891477e757b"
-#set SPOTIPY_REDIRECT_URI=Enter your redirect URL here
+redirect_url = "https://www.google.com/"
+username = "j389o9xb4nnxfn1fvoftubvst"
+scope = 'user-read-private user-read-playback-state user-modify-playback-state'
+
 client_credentials_manager = SpotifyClientCredentials(client_id = cid,client_secret = secret_id)
 sp = Spotify(client_credentials_manager=client_credentials_manager)
-
-username = sys.argv[1]
-scope = 'user-read-private user-read-playback-state user-modify-playback-state'
 
 
 ## playing music using my virtual assistant
 try:
-    token = util.prompt_for_user_token(username, scope)
+    token = util.prompt_for_user_token(username, scope,cid,secret_id,redirect_url)
 except (AttributeError):
     os.remove(f".cache-{username}")
-    token = util.prompt_for_user_token(username, scope)
+    token = util.prompt_for_user_token(username, scope,cid,secret_id,redirect_url)
+
+spotify_object = spotipy.Spotify(auth=token)
 
 # setting up the dataframe
 artist_name = []
@@ -52,6 +55,7 @@ track_id = []
 #         track_name.append("")
 #         track_id.append("")
 
+
 artist_search = "The Weeknd"
 artist_results = sp.search(q = f"artist:{artist_search}", type = "artist", limit = 1)
 
@@ -68,6 +72,50 @@ track_dataframe = pd.DataFrame({"artist_name":artist_name,"track_name":track_nam
 new_df = track_dataframe.sort_values("popularity",ascending=False)
 new_df = new_df.drop_duplicates("track_id")
 print(new_df.head(20))
+
+# Print artist details
+searchResults = sp.search(artist_search,1,0,"artist")
+artist = searchResults['artists']['items'][0]
+print(artist['name'])
+
+webbrowser.open(artist['images'][0]['url'])
+artistID = artist['id']
+
+# Extract data from album
+album_results = sp.artist_albums(artistID)
+album_results = album_results['items']
+
+ # Album details
+trackURIs = []
+trackArt = []
+i = 0
+
+devices = spotify_object.devices()
+deviceID = devices['devices'][0]['id']
+
+for item in album_results:
+    print("ALBUM: " + item['name'])
+    albumID = item['id']
+    albumArt = item['images'][0]['url']
+
+    # Extract track data
+    trackResults = sp.album_tracks(albumID)
+    trackResults = trackResults['items']
+
+    for item in trackResults:
+        print(str(i) + ": " + item['name'])
+        trackURIs.append(item['uri'])
+        trackArt.append(albumArt)
+        i += 1
+    print()
+
+# See album art
+while True:
+    trackSelectionList = []
+    trackSelectionList.append(trackURIs[int(181)])
+    sp.start_playback(deviceID, None, trackSelectionList)
+    webbrowser.open(trackArt[int(181ff)])
+
 
 
 
