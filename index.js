c253b1bec2cca5eb357f50312ae55c39dbea466d
Sechaba Mokobane <sechaba.mokobane@gmail.com>
2021-06-05 21:40:28 +0200

    Added dynamism to the image panel using jQuery

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-// alert("Welcome to the index page")
-'use strict'
+// 'use strict'
 
 // Using jQuery to check if the html doc is in a ready state i.e. DOM loaded
-$document.ready(function(){
+// $document.ready(function () {
+//     $.
+// })
 
-})
+
+let artist_images = $("img")
+let imgs_num = artist_images.length
+
+// artist_img.hover(function () {
+//     artist_img.eq(1).attr("src", "images/nicki_minaj.jpg")
+//     artist_img.setAttribute("class", "img-thumbnail")
+// })
+
+
+function change_pic() {
+    let random_num = Math.floor(Math.random() * imgs_num)
+    console.log(artist_images.eq(Number(random_num)))
+    // artist_images.eq(0).attr("src", artist_images.eq(Number(random_num)))
+    // artist_images.attr("class", "img-thumbnail")
+}
+change_pic()
+// setInterval(200, change_pic)
