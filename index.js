4e074c7278c096fb0db429cc3ea18be953d7b5c4
Sechaba Mokobane <sechaba.mokobane@gmail.com>
2021-06-22 21:40:26 +0200

    Frustrating day trying to line up the images and 'tweet' area - used the display property in the end

--- index.js
+++ index.js
@@ -2,16 +2,11 @@
 
 // Using jQuery to check if the html doc is in a ready state i.e. DOM loaded
 $(document).ready(function () {
-    let artist_images = $("img")
-
-    artist_images.hover(function () {
-        artist_images.eq(1).attr("src", "images/nicki_minaj.jpg")
-        artist_images.eq(1).attr("class", "img-thumbnail")
-    })
-
-
+  // let artist_images = $("img")
+  // artist_images.hover(function () {
+  //     artist_images.eq(1).attr("src", "images/nicki_minaj.jpg")
+  //     artist_images.eq(1).attr("class", "img-thumbnail")
+  // })
 })
-
-
 
 // setInterval(200, change_pic)
